50bb024f6c056c29d8230b09a5fe57176334f55a
DSlash45 <64011196@kmitl.ac.th>
2025-02-06 03:58:50 +0700

    Add files via upload

---
+++ cell00/ex04/ex04/argv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+count=0
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    for arg in "$@"; do
+        echo "$arg"
+        count=$((count + 1))
+        if [ $count -ge 3 ]; then 
+            break 
+        fi
+    done
+fi
